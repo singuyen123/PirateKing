627477239430a863cfa08be5248e500c3774f29e
nambang <nambangn@gmail.com>
2018-11-02 14:50:00 +0000

    connect code Bang Vs si

--- Bang/NUC131BSP-master/Code/StdDriver/UART_TxRx_Function/KEIL/obj/startup_nuc131.d
+++
@@ -1,1 +0,0 @@
-.\obj\startup_nuc131.o: ..\..\..\..\Library\Device\Nuvoton\NUC131\Source\ARM\startup_NUC131.s
--- Bang/NUC131BSP-master/Code/StdDriver/UART_TxRx_Function/KEIL/obj/system_nuc1xx.d
+++
@@ -1,3 +0,0 @@
-.\obj\system_nuc1xx.o: ..\..\..\..\Library\Lib\CMSIS\CM0\DeviceSupport\Nuvoton\NUC1xx\system_NUC1xx.c
-.\obj\system_nuc1xx.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
-.\obj\system_nuc1xx.o: ..\..\..\..\Library\Lib\CMSIS\CM0\DeviceSupport\Nuvoton\NUC1xx\NUC1xx.h
