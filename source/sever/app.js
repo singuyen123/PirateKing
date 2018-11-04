94422fc0024f7e77374bbe7a0a4f7860d194deae
singuyen123 <15520726@gm.uit.edu.vn>
2018-11-04 15:54:57 +0700

    add user to database

--- source/sever/app.js
+++ source/sever/app.js
@@ -6,7 +6,8 @@ var express = require('express');
 //var path = require('path');
 var app = express();
 var WebSocketServer = require('ws').Server, wss = new WebSocketServer({ port: 80 })
-
+var username
+var pass
 
 app.get('/', function (req, res) {
   res.sendfile('/home/sideptr/workspace/PirateKing/source/html/signin.html');
@@ -18,33 +19,58 @@ app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
 
+app.post('/submit', function (req, res) {
+  checkLoginAccount(username, pass, function (result) {
+      if (result) {
+          res.redirect('/play');
+      } 
+  });
+});
 
+function checkLoginAccount(username, password, callback) {
+  MongoClient.connect(mongodbUrl, function (err, db) {
+      assert.equal(null, err);
+
+      var querryObj = { 'username': username };
+
+      db.collection("user").findOne(querryObj, function (err, result) {
+          assert.equal(null, err);
+
+          if ((result) && (result.pass == password)) {
+              callback(true);
+          } else {
+              callback(false);
+          }
+
+          db.close();
+      });
+  });
+}
 // fired when the mqtt server is ready
 
 wss.on('connection', function (ws) {
   ws.on('message', function (message) {
     var message_json = JSON.parse(message);
-    console.log(message_json.stt);
-    console.log(message_json);
     if (message_json.stt == 'login') {
-
+      username = message_json.username
+      pass = message_json.pass
     }
-    else if(message_json.stt == 'signup'){
+    else if (message_json.stt == 'signup') {
       console.log(message_json.username);
       MongoClient.connect(mongodbUrl, function (err, db) {
         assert.equal(null, err);
         //var txt = db.collection('mycollection').findOne();
         //console.log(txt);
-        // db.collection("mycollection").find({}).toArray(function(err, result) {
+        // db.collection("mycollection").find({"by":"tutorials point"}).toArray(function(err, result) {
         //   if (err) throw err;
         //   console.log(result);
         //   db.close();
         // });
         db.collection("user").insert({
-          username: message_json.username, 
+          username: message_json.username,
           pass: message_json.pass,
           email: message_json.email
-       })
+        })
       });
     }
   })
